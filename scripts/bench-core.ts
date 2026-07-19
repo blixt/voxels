0856e24e50a49cf12af418fbac93cadb67540faf
Blixt <me@blixt.nyc>
2026-07-19 12:22:52 +0200

    Remove obsolete automation entry points

--- scripts/bench-core.ts
+++
@@ -1,6 +0,0 @@
-import { execFileSync } from "node:child_process";
-import { rustTool } from "./build-wasm.ts";
-
-execFileSync(rustTool("cargo"), ["bench", "-p", "voxels-core", "--bench", "simulation"], {
-  stdio: "inherit",
-});
